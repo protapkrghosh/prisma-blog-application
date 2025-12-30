d619cd3c6b4ad058005fb6d946a3a83e7612bb4b
protapkrghosh <protapg518@gmail.com>
2025-12-30 20:29:27 +0600

    added authentication

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
+import { auth } from "./lib/auth";
 import express, { type Application } from "express";
 import { postRouter } from "./modules/post/post.route";
+import { toNodeHandler } from "better-auth/node";
+import cors from "cors";
 
 const app: Application = express();
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
-
-app.use('/posts', postRouter)
+app.use(
+   cors({
+      origin: process.env.APP_URL || "http://localhost:4000",
+      credentials: true,
+   })
+);
+app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
    res.send("Welcome To Blog Application");
