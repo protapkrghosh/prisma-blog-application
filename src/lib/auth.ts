ba3a83e61c0c1794090c0792754df6a2fa2469f8
protapkrghosh <protapg518@gmail.com>
2025-12-31 00:04:31 +0600

    added nodemailer

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,17 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+   host: "smtp.gmail.com",
+   port: 587,
+   secure: false, // Use true for port 465, false for port 587
+   auth: {
+      user: process.env.APP_USER,
+      pass: process.env.APP_PASS,
+   },
+});
 
 export const auth = betterAuth({
    database: prismaAdapter(prisma, {
@@ -32,5 +43,19 @@ export const auth = betterAuth({
       enabled: true,
       autoSignIn: false,
       requireEmailVerification: true,
+   },
+
+   emailVerification: {
+      sendVerificationEmail: async ({ user, url, token }, request) => {
+         const info = await transporter.sendMail({
+            from: '"Prisma Blog" <prismablog@pr.com>',
+            to: "protapghosh937@gmail.com",
+            subject: "Hello ✔",
+            text: "Hello world?",
+            html: "<b>Hello world?</b>",
+         });
+
+         console.log("Message sent:", info.messageId);
+      },
    },
 });
