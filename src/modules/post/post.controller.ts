ce6af16cc2fc64803af9fe40a4b56315adc95aae
protapkrghosh <protapg518@gmail.com>
2026-01-05 15:53:02 +0600

    filter by author id

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -39,12 +39,17 @@ const getAllPost = async (req: Request, res: Response) => {
       const status = req.query.status as PostStatus | undefined;
       const authorId = req.query.authorId as string | undefined;
 
+      const page = Number(req.query.page ?? 1);
+      const limit = Number(req.query.limit ?? 10);
+
       const result = await postService.getAllPost({
          search: searchString,
          tags,
          isFeatured,
          status,
          authorId,
+         page,
+         limit,
       });
       res.status(200).json(result);
    } catch (error) {
---
+++ src/scripts/seedAdmin.ts
@@ -0,0 +1,51 @@
+import { prisma } from "../lib/prisma";
+import { UserRole } from "../middleware/auth";
+
+async function seedAdmin() {
+   try {
+      const adminData = {
+         name: process.env.ADMIN_NAME,
+         email: process.env.ADMIN_EMAIL,
+         role: UserRole.ADMIN,
+         password: process.env.ADMIN_PASSWORD,
+         emailVerified: true,
+      };
+
+      // Check user exist on database or not
+      const existingUser = await prisma.user.findUnique({
+         where: {
+            email: adminData.email as string,
+         },
+      });
+
+      if (existingUser) {
+         throw new Error("User already exists!");
+      }
+
+      const signUpAdmin = await fetch(
+         "http://localhost:3000/api/auth/sign-up/email",
+         {
+            method: "POST",
+            headers: {
+               "content-type": "application/json",
+            },
+            body: JSON.stringify(adminData),
+         }
+      );
+
+      if (signUpAdmin.ok) {
+         await prisma.user.update({
+            where: {
+               email: adminData.email as string,
+            },
+            data: {
+               emailVerified: true,
+            },
+         });
+      }
+   } catch (error) {
+      console.error(error);
+   }
+}
+
+seedAdmin();
